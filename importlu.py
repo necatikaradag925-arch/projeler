4eda3e2dbf9615eb2d65d22d512dccaccc3e776d
necatikaradag925-arch <necatikaradag925@gmail.com>
2026-05-13 20:59:59 +0300

    sayi tahmin machıne

---
+++ importlu.py
@@ -0,0 +1,10 @@
+import random
+
+def rastgele_sayi_tut():
+    tuttuğun_sayi = random.randint(1, 100)
+    return tuttuğun_sayi
+
+tuttuğun_sayi = rastgele_sayi_tut()
+print("Tuttuğun sayı:", tuttuğun_sayi)
+
+  
