97cb2c310dd52bb91f54bf12abbb8ea611682326
necatikaradag925-arch <necatikaradag925@gmail.com>
2026-06-16 12:10:23 +0300

    vki_hesaplama

---
+++ vki.py
@@ -0,0 +1,25 @@
+kilo = float(input("kaç kilo olduğunuzu giriniz:")) 
+boy = float(input("boyunuzu metre cinsinden giriniz :"))        
+
+  
+
+ 
+
+vki = kilo / (boy * boy)
+        
+    
+
+print(f"vücut kitle indeksiniz: {vki:.2f}")     
+
+    
+if vki < 18.5:
+    print("zayıf")
+
+elif vki >= 18.5 and vki < 25:
+    print("normal")
+
+elif vki >= 25 and vki < 29.9:  
+    print("kilolu")
+
+else:
+    print("obez")   
